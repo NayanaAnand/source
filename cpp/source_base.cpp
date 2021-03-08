b6be8bcf7eeb94270bb15e40fbf1851fd9bfddc0
Nayana N R <nayana.nr@cyient.com>
2021-03-08 15:18:24 +0530

    [source] : Modified configure.ac

--- cpp/source_base.cpp
+++ cpp/source_base.cpp
@@ -14,7 +14,7 @@ source_base::source_base(const char *uuid, const char *label) :
     Component(uuid, label),
     ThreadedComponent()
 {
-    setThreadName(label);
+    //setThreadName(label);
 
     loadProperties();
 
